7a770ecc2c6b643d5819b2b1e49fbec27fea4915
bharathtakkalki <bharathtakkalki@gmail.com>
2019-09-07 00:44:38 +0530

    Added SignUp tab to the Modal - Implementation is done as per the deisgn - Styling has been added  as per the design - Helpertext are added for signup & login tabs

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -26,6 +26,15 @@ const styles = (theme => ({
     loginButton: {
         "font-weight": 400,
     },
+    formButton:{
+        "font-weight": 400,
+    },
+    tab:{
+        "font-weight": 400,
+    },
+    formControl:{
+        "width":"80%",
+    }
 
 }))
 const customStyles = {
@@ -40,7 +49,7 @@ const customStyles = {
 };
 const TabContainer = function (props) {
     return (
-        <Typography component="div" style={{ padding: 0, textAlign: 'center' }}>
+        <Typography component="div" style={{ padding: '0px', textAlign: 'center' }}>
             {props.children}
         </Typography>
     )
@@ -59,11 +68,25 @@ class Header extends Component {
         this.state = {
             isModalOpen: false,
             value: 0,
-            contactNo: "",
-            contactNoRequired: "dispNone",
+            loginContactNo: "",
+            loginContactNoRequired: "dispNone",
             loginPassword: "",
             loginPasswordRequired: "dispNone",
-
+            firstName:"",
+            firstNameRequired:"dispNone",
+            lastName:"",
+            email:"",
+            emailRequired:"dispNone",
+            signUpPassword:"",
+            signUpPasswordRequired:"dispNone",
+            signUpContactNo:"",
+            signUpContactNoRequired:"dispNone",
+            inValidContact:"dispNone",
+            invalidPassword:"dispNone",
+            notRegisteredContact:"dispNone",
+            validPasswordHelpText:"dispNone",
+            contactNoRegistered:"dispNone",
+            contactHelpText:"dispNone",
 
         }
 
@@ -90,21 +113,52 @@ class Header extends Component {
         })
     }
 
-    inputContactNoChangeHandler = (event) => {
+    inputLoginContactNoChangeHandler = (event) => {
         this.setState({
             ...this.state,
-            contactNo:event.target.value,
+            loginContactNo:event.target.value,
         })
     }
 
-    inputPasswordChangeHandler = (event) => {
+    inputLoginPasswordChangeHandler = (event) => {
         this.setState({
             ...this.state,
             loginPassword:event.target.value,
         })
     }
 
-    tabChangeHandler = (event, value) => {
+    inputFirstNameChangeHandler = (event) => {
+        this.setState({
+            ...this.state,
+            firstName:event.target.value,
+        })
+    }
+    inputLastNameChangeHandler = (event) => {
+        this.setState({
+            ...this.state,
+            lastName:event.target.value,
+        })
+    }
+    inputEmailChangeHandler = (event) => {
+        this.setState({
+            ...this.state,
+            email:event.target.value,
+        })
+    }
+    inputSignUpPasswordChangeHandler = (event) => {
+        this.setState({
+            ...this.state,
+            signUpPassword:event.target.value,
+        })
+    }
+    inputSignUpContactNoChangeHandler = (event) => {
+        this.setState({
+            ...this.state,
+            signUpContactNo:event.target.value,
+        })
+    }
+
+    tabsChangeHandler = (event, value) => {
         this.setState({ 
             value });
     }
@@ -131,40 +185,107 @@ class Header extends Component {
                 </header>
                 <Modal
                     ariaHideApp={false}
-                    ariaHideApp={false}
                     isOpen={this.state.isModalOpen}
                     contentLabel="Login"
                     onRequestClose={this.closeModalHandler}
                     style={customStyles}
                 >
                     <Tabs className="login-modal-tabs" value={this.state.value} onChange={this.tabsChangeHandler}>
-                        <Tab label="LOGIN" />
-                        <Tab label="SIGNUP" />
+                        <Tab label="LOGIN" className= {classes.tab}/>
+                        <Tab label="SIGNUP" className= {classes.tab}/>
                     </Tabs>
 
                     {this.state.value === 0 &&
                         <TabContainer>
-                            <FormControl required>
-                                <InputLabel htmlFor="contact-no">Contact No.</InputLabel>
-                                <Input id="contact-no" className = "input-fields" type="text" contactNo={this.state.contactNo} onChange={this.inputContactNoChangeHandler}/>
-                                <FormHelperText className={this.state.contactNoRequired}>
+                            <FormControl required className={classes.formControl}>
+                                <InputLabel htmlFor="login-contact-no">Contact No.</InputLabel>
+                                <Input id="login-contact-no" className = "input-fields" fullWidth={true} type="text" logincontactno={this.state.loginContactNo} onChange={this.inputLoginContactNoChangeHandler}/>
+                                <FormHelperText className={this.state.loginContactNoRequired}>
                                     <span className='red'>required</span>
                                 </FormHelperText>
+                                <FormHelperText className={this.state.inValidContact}>
+                                    <span className="red">Invalid Contact</span>
+                                </FormHelperText>
+
                             </FormControl>
                             <br />
                             <br />
-                            <FormControl required>
+                            <FormControl required className={classes.formControl}>
                                 <InputLabel htmlFor="login-password">Password</InputLabel>
-                                <Input id="login-password" className = "input-fields" type="password" loginPassword={this.state.password} onChange={this.inputPasswordChangeHandler} />
+                                <Input id="login-password" className = "input-fields" type="password" loginpassword={this.state.loginPassword} fullWidth={true} onChange={this.inputLoginPasswordChangeHandler} />
                                 <FormHelperText className={this.state.loginPasswordRequired}>
                                     <span className='red'>required</span>
                                 </FormHelperText>
+                                <FormHelperText className={this.state.invalidPassword}>
+                                    <span className="red">Invalid Credentials</span>
+                                </FormHelperText>
+                                <FormHelperText className={this.state.notRegisteredContact}>
+                                    <span className="red">This contact number has not been registered!</span>
+                                </FormHelperText>
                             </FormControl>
                             <br/>
                             <br/>
                             <br/>
-                            <Button variant="contained" color="primary" onClick={this.loginClickHandler}>LOGIN</Button>
+                            <Button variant="contained" className = {classes.formButton} color="primary" onClick={this.loginClickHandler}>LOGIN</Button>
                         </TabContainer> 
+                    }
+                    {this.state.value === 1 &&
+                        <TabContainer>
+                            <FormControl required className={classes.formControl}>
+                                <InputLabel htmlFor="first-name">First Name</InputLabel>
+                                <Input id ="first-name" className="input-fields" firstname={this.state.firstName} fullWidth={true} onChange = {this.inputFirstNameChangeHandler}/>
+                                <FormHelperText className={this.state.firstNameRequired}>
+                                    <span className = "red">required</span>
+                                </FormHelperText>
+                            </FormControl>
+                            <br/>
+                            <br/>
+                            <FormControl className={classes.formControl}>
+                                <InputLabel htmlFor="last-name">Last Name</InputLabel>
+                                <Input id ="last-name" className="input-fields" lastname={this.state.lastName} fullWidth={true} onChange = {this.inputLastNameChangeHandler}/>
+                            </FormControl>
+                            <br/>
+                            <br/>
+                            <FormControl required className={classes.formControl}>
+                                <InputLabel htmlFor="email">Email</InputLabel>
+                                <Input id ="email" className="input-fields" type="email" email={this.state.email} fullWidth={true} onChange = {this.inputEmailChangeHandler}/>
+                                <FormHelperText className={this.state.emailRequired}>
+                                    <span className = "red">required</span>
+                                </FormHelperText>
+                            </FormControl>
+                            <br/>
+                            <br/>
+                            <FormControl required className={classes.formControl}>
+                                <InputLabel htmlFor="sign-up-password">Password</InputLabel>
+                                <Input id ="sign-up-password" className="input-fields" signuppassword={this.state.signUpPassword} fullWidth={true} onChange = {this.inputSignUpPasswordChangeHandler}/>
+                                <FormHelperText className={this.state.signUpPasswordRequired}>
+                                    <span className = "red">required</span>
+                                </FormHelperText>
+                                <FormHelperText className={this.state.validPasswordHelpText}>
+                                    <span className="red">Password must contain at least one capital letter, one small letter, one number, and one special character</span>
+                                </FormHelperText>
+                            </FormControl>
+                            <br/>
+                            <br/>
+                            <FormControl required className={classes.formControl}>
+                                <InputLabel htmlFor="sign-up-contactNo">Contact No.</InputLabel>
+                                <Input id ="sign-up-contactNo" className="input-fields" signupcontactno={this.state.signUpContactNo} fullWidth={true} onChange = {this.inputSignUpContactNoChangeHandler}/>
+                                <FormHelperText className={this.state.signUpContactNoRequired}>
+                                    <span className = "red">required</span>
+                                </FormHelperText>
+                                <FormHelperText className={this.state.contactHelpText}>
+                                    <span className="red">Contact No. must contain only numbers and must be 10 digits long</span>
+                                </FormHelperText>
+                                <FormHelperText className={this.state.contactNoRegistered}>
+                                    <span className="red">This contact number is already registered! Try other contact number.</span>
+                                </FormHelperText>
+                            </FormControl>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <Button variant="contained" className ={classes.formButton}color="primary" onClick={this.signUpClickHandler}>SIGNUP</Button>
+                        </TabContainer>
+
                     }
                 </Modal>
 
