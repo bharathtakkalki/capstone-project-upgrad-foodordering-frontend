4f007f337fe3ab3c085905fb513d355e63944596
bharathtakkalki <bharathtakkalki@gmail.com>
2019-09-06 17:26:23 +0530

    Created basic folder structure & added router - Created controller class for routing to different pages - Created screen,home & header folder along with css & js files - Added react-router-dom

---
+++ src/screens/home/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+
+
+class Home extends Component {
+
+    render() {
+        return(
+            <div>
+                Food Ordering app.
+            </div>
+        )
+
+
+
+
+    }
+
+
+}
+
+export default Home;
