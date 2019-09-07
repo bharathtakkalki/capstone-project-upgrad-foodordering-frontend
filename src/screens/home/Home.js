0f1cae135693d9dfd35aee8d423b67f6e2197849
bharathtakkalki <bharathtakkalki@gmail.com>
2019-09-07 23:23:27 +0530

    Added Grid to the Home page - Grid shows no of cards as per the screen - Card Sizes adjusts form 4 to one based on screen size

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
 import Header from '../../common/header/Header';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+
+import './Home.css';
+
+const styles = (theme => ({
 
 
+    grid: { //style for the grid component 
+        "padding-top":"20px",
+        "margin-left": "0.5%",
+        "margin-right": "0.5%",
+    },
+
+
+
+
+}))
 
 
 class Home extends Component {
 
     render() {
-        return(
+        const { classes } = this.props;
+        return (
             <div>
+
                 <Header baseUrl={this.props.baseUrl}></Header>
-                Food Ordering app.
+                <div className="flex-container">
+                    <Grid container spacing={3} wrap="wrap" alignContent="center" className={classes.grid}>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Paper className="paper">xs=6 sm=3</Paper>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Paper className="paper">xs=6 sm=3</Paper>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Paper className="paper">xs=6 sm=3</Paper>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Paper className="paper">xs=6 sm=3</Paper>
+                        </Grid>
+                    </Grid>
+                    Food Ordering app.
+                </div>
             </div>
         )
 
@@ -22,4 +57,4 @@ class Home extends Component {
 
 }
 
-export default Home;
+export default withStyles(styles) (Home);
