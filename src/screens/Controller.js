2457912dc95d272181f97263f09b55a085c52a9d
bharathtakkalki <bharathtakkalki@gmail.com>
2019-09-30 12:45:37 +0530

    Added Comments to the Header File - added relevant comments to the file

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -4,18 +4,18 @@ import Home from './home/Home';
 
 
 
-
+//Creating controller class for easy routing the pages
 class Controller extends Component{
     constructor(){
         super()
-        this.baseUrl = "http://localhost:8080/api/"
+        this.baseUrl = "http://localhost:8080/api/" //setting the baseUrl of the api
     }
 
     render(){
         return(
             <Router>
                 <div className = 'main-conatiner'>
-                    <Route exact path = '/' render={(props) => <Home {...props} baseUrl = {this.baseUrl}/>}/>
+                    <Route exact path = '/' render={(props) => <Home {...props} baseUrl = {this.baseUrl}/>}/> {/* Route to home Page */ }
                 </div>
             </Router>
 
